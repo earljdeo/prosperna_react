3d2636fb6545129f944e5095117c5aa3a1e3ae74
earloutliant <deo@outliant.com>
2022-10-12 18:28:20 +0800

    - Test Push

---
+++ src/Components/HeroContent.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import headingimg1 from '../imgs/heading-hero.svg'
+function HeroContent() {
+    return (
+        <div className="float-content">
+            <img src={headingimg1} className="img-fluid" />
+            <img src={headingimg1} className="img-fluid" />
+            <img src={headingimg1} className="img-fluid" />
+            <img src={headingimg1} className="img-fluid" />
+            <img src={headingimg1} className="img-fluid" />
+            <img src={headingimg1} className="img-fluid" />
+        </div>
+    )
+}
+export default HeroContent;
